7d034e3bff6738dbb9d78bbafa773e8b78c395f1
stefv <svanpo@gmail.com>
2017-04-26 21:40:30 +0200

    Fix the variable names.

--- package/build.sh
+++ package/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 VERSION=0.0.1-alpha1
 ARCH=armhf
-FILENAME=mhs-$(VERSION)_$(ARCH).deb
+FILENAME=mhs-${VERSION}_${ARCH}.deb
 
 # Create the package
-dpkg-deb --build mhs $FILENAME
+dpkg-deb --build mhs ${FILENAME}
 
 # Help message
-echo To install: sudo dpkg -i $FILENAME
+echo To install: sudo dpkg -i ${FILENAME}
 
 exit 0
