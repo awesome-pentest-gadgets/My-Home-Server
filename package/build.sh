58bec71b8a787c5b15a7ec2a98253af47defa589
stefv <svanpo@gmail.com>
2017-04-26 12:51:54 +0200

    Remove errors from the empty data directory.

--- package/build.sh
+++ package/build.sh
@@ -10,7 +10,7 @@ if [ ! -d "data" ]; then
         mkdir data
 fi
 cd data
-tar -cvzf ../target/data.tar.gz *
+tar -cvzf ../target/data.tar.gz * &>/dev/null
 cd ..
 
 # Create the debian-binary file
