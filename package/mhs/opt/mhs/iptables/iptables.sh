3d82f301992a3975b2c869aada6f261ea219f02a
stefv <svanpo@gmail.com>
2017-04-26 21:59:59 +0200

    Log the inputs.

--- package/mhs/opt/mhs/iptables/iptables.sh
+++ package/mhs/opt/mhs/iptables/iptables.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-iptables -F INPUT
-iptables -A INPUT -p tcp --dport ssh -j ACCEPT
-iptables -P INPUT DROP
+# Delete all the existing rules
+iptables -F
 
-iptables -N LOG_DROP
-iptables -A LOG_DROP -j LOG --log-prefix '[IPTABLES DROP] : '
-iptables -A LOG_DROP -j DROP
+# Set default chain policies
+#iptables -P INPUT DROP
+#iptables -P FORWARD DROP
+
+#iptables -A INPUT -p tcp --dport ssh -j ACCEPT
+#iptables -P INPUT DROP
+
+# Log all the requests
+#iptables -N LOG_DROP
+#iptables -A LOG_DROP -j LOG --log-prefix '[IPTABLES DROP] : '
+#iptables -A LOG_DROP -j DROP
+
+iptables -A INPUT -j LOG --log-prefix='[netfilter] '
 
 exit 0
