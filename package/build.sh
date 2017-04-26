4f0adbf809d404fd22a937c0f81c0bbc6c065944
stefv <svanpo@gmail.com>
2017-04-26 21:37:29 +0200

    Add help to the build script.

--- package/build.sh
+++ package/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+VERSION=0.0.1-alpha1
+ARCH=armhf
+FILENAME=mhs-$(VERSION)_$(ARCH).deb
 
 # Create the package
-dpkg-deb --build mhs mhs-0.0.1-alpha1_armhf.deb
+dpkg-deb --build mhs $FILENAME
+
+# Help message
+echo To install: sudo dpkg -i $FILENAME
 
 exit 0
