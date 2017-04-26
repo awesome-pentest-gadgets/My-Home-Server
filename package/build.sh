c3bb0eada6ae9b536d7b9a75558c384971acd5e1
stefv <svanpo@gmail.com>
2017-04-26 09:03:45 +0200

    Create the debian-binary file.

--- package/build.sh
+++ package/build.sh
@@ -8,7 +8,11 @@ fi
 # Create the control.tar.gz file
 cd control
 tar -cvzf ../target/control.tar.gz *
-
 cd ..
+
+# Create the debian-binary file
+if [ ! -a target/debian-binary ]; then
+	printf "2.0\n" > target/debian-binary
+fi
 
 exit 0
