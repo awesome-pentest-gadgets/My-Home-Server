bc8691c049480c613e74d46e747fb247f2aff872
stefv <svanpo@gmail.com>
2017-04-27 07:15:42 +0200

    Update the domain iptables rules.

--- package/mhs/opt/mhs/iptables/iptables.sh
+++ package/mhs/opt/mhs/iptables/iptables.sh
@@ -12,7 +12,11 @@ iptables -A INPUT -p tcp --dport ssh -j ACCEPT
 iptables -A INPUT -p tcp --dport https -j ACCEPT
 iptables -A INPUT -p tcp --sport https -j ACCEPT
 
-iptables -A INPUT -p udp --sport domain -j ACCEPT
+# Accept the DNS requests
+iptables -A INPUT --protocol udp --source-port domain -j ACCEPT
+iptables -A OUTPUT --protocol udp --destination-port domain -j ACCEPT
+iptables -A INPUT --protocol tcp --source-port domain -j ACCEPT
+iptables -A OUTPUT --protocol tcp --destination-port domain -j ACCEPT
 
 # Logs the unknowns inputs
 iptables -A INPUT -j LOG --log-prefix='[netfilter] '
