3a0b6e0ed3dc80d479b3dc531eac9f4a6bf3384d
stefv <svanpo@gmail.com>
2017-04-30 12:02:41 +0200

    Search the war filename to give to Tomcat automatically.

--- mhs_server/src/main/java/mhs/server/Server.java
+++ mhs_server/src/main/java/mhs/server/Server.java
@@ -1,11 +1,11 @@
 package mhs.server;
 
 import java.io.File;
-
-import javax.servlet.ServletException;
+import java.net.URL;
 
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.tomcat.util.http.fileupload.FileUtils;
 
 /**
  * The server.
@@ -24,41 +24,35 @@ public class Server {
         server.startTomcat();
     }
 
-    private void startTomcat() throws ServletException, LifecycleException {
-        final String webappDirLocation = "./mhs_server.war";
+    /**
+     * Start the Tomcat server.
+     * 
+     * @throws Exception Exception.
+     */
+    private void startTomcat() throws Exception {
+
+        final Class<?> serverClass = Server.class;
+        final URL location = serverClass.getResource('/' + serverClass.getName().replace('.', '/') + ".class");
+        if (!"jar".equals(location.getProtocol())) {
+            throw new RuntimeException("The file is not a JAR or WAR.");
+        }
+        final File warFile = new File(location.getFile().split("!")[0]);
+        final String webappDirLocation = warFile.getName();
+
+        // If a previous webapps directory exist, remove it to have a clean install
+        final File webappsDir = new File("./webapps");
+        if (webappsDir.exists()) {
+            FileUtils.forceDelete(webappsDir);
+        }
+
+        // Create a webapps directory because Tomcat need it to unzip the webapp
+        webappsDir.mkdir();
+
+        // Start the server
         final Tomcat tomcat = new Tomcat();
-
-        new File("./webapps").mkdir();
-
         tomcat.setBaseDir("./");
         tomcat.addWebapp("", new File(webappDirLocation).getAbsolutePath());
-
         tomcat.start();
         tomcat.getServer().await();
     }
-
-    // System.out.println("User dir: " + System.getProperty("user.dir"));
-    // System.out.println("Base dir: " + System.getProperty("java.io.tmpdir"));
-
-    // final String webappDirLocation = "src/main/webapp/";
-    // The port that we should run on can be set into an environment variable
-    // Look for that variable and default to 8080 if it isn't there.
-    // String webPort = System.getenv("PORT");
-    // if (webPort == null || webPort.isEmpty()) {
-    // webPort = "8080";
-    // }
-    //
-    // tomcat.setPort(Integer.valueOf(webPort));
-
-    /*
-     * final StandardContext ctx = (StandardContext) tomcat.addWebapp("", new File(webappDirLocation).getAbsolutePath());
-     * System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath()); // Declare an
-     * alternative location for your "WEB-INF/classes" dir // Servlet 3.0 annotation will work final File additionWebInfClasses = new
-     * File("target/classes"); final WebResourceRoot resources = new StandardRoot(ctx); resources.addPreResources(new
-     * DirResourceSet(resources, "/WEB-INF/classes", additionWebInfClasses.getAbsolutePath(), "/")); ctx.setResources(resources);
-     */
-
-    // final URL location = Server.class.getResource('/' + Server.class.getName().replace('.', '/') + ".class");
-    // Server.class.getResource(name)
-
 }
