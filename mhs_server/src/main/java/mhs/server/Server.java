2df50652e9df08715ca5a9d136c07928e554cf75
stefv <svanpo@gmail.com>
2017-04-30 10:37:36 +0200

    First version with embedded Tomcat working.

--- mhs_server/src/main/java/mhs/server/Server.java
+++ mhs_server/src/main/java/mhs/server/Server.java
@@ -1,5 +1,7 @@
 package mhs.server;
 
+import java.io.File;
+
 import javax.servlet.ServletException;
 
 import org.apache.catalina.LifecycleException;
@@ -14,20 +16,49 @@ public class Server {
      * The main method of the server.
      * 
      * @param args Arguments.
-     * @throws ServletException
+     * @throws Exception
      * @throws LifecycleException
      */
-    public static void main(final String[] args) throws ServletException, LifecycleException {
-
-        System.out.println("User dir: " + System.getProperty("user.dir"));
-        System.out.println("Base dir: " + System.getProperty("java.io.tmpdir"));
-
-        final Tomcat server = new Tomcat();
-        server.setPort(8080);
-        server.setBaseDir(System.getProperty("user.dir"));
-        server.addWebapp("", "mhs_server.war");
-        server.start();
-
-        server.getServer().await();
+    public static void main(final String[] args) throws Exception {
+        final Server server = new Server();
+        server.startTomcat();
     }
+
+    private void startTomcat() throws ServletException, LifecycleException {
+        final String webappDirLocation = "./mhs_server.war";
+        final Tomcat tomcat = new Tomcat();
+
+        new File("./webapps").mkdir();
+
+        tomcat.setBaseDir("./");
+        tomcat.addWebapp("", new File(webappDirLocation).getAbsolutePath());
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+
+    // System.out.println("User dir: " + System.getProperty("user.dir"));
+    // System.out.println("Base dir: " + System.getProperty("java.io.tmpdir"));
+
+    // final String webappDirLocation = "src/main/webapp/";
+    // The port that we should run on can be set into an environment variable
+    // Look for that variable and default to 8080 if it isn't there.
+    // String webPort = System.getenv("PORT");
+    // if (webPort == null || webPort.isEmpty()) {
+    // webPort = "8080";
+    // }
+    //
+    // tomcat.setPort(Integer.valueOf(webPort));
+
+    /*
+     * final StandardContext ctx = (StandardContext) tomcat.addWebapp("", new File(webappDirLocation).getAbsolutePath());
+     * System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath()); // Declare an
+     * alternative location for your "WEB-INF/classes" dir // Servlet 3.0 annotation will work final File additionWebInfClasses = new
+     * File("target/classes"); final WebResourceRoot resources = new StandardRoot(ctx); resources.addPreResources(new
+     * DirResourceSet(resources, "/WEB-INF/classes", additionWebInfClasses.getAbsolutePath(), "/")); ctx.setResources(resources);
+     */
+
+    // final URL location = Server.class.getResource('/' + Server.class.getName().replace('.', '/') + ".class");
+    // Server.class.getResource(name)
+
 }
