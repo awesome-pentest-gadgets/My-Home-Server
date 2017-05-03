3ad464acbcf8477b652c6133a70bf3e0f631f43a
stefv <svanpo@gmail.com>
2017-05-03 13:07:09 +0200

    Add the war building.

--- package/build.sh
+++ package/build.sh
@@ -3,6 +3,19 @@ VERSION=0.0.1-alpha1
 ARCH=armhf
 FILENAME=mhs-${VERSION}_${ARCH}.deb
 
+# Create the webapp
+pushd .
+cd ../mhs_server/
+mvn clean install
+result=$?
+popd
+if [ $status -ne 0 ]; then
+	echo Can''t create the webapp.
+	exit 1
+else
+	cp ../mhs_server/target/*.war ./
+fi
+
 # Create the package
 dpkg-deb --build mhs ${FILENAME}
 
